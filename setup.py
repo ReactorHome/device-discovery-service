1a81822324aad38b3cc3222ecdb5250d5741c3ed
Michael Smith <mdsf3f@mail.missouri.edu>
2018-03-20 19:25:18 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hue-api',
+    version='1.0.0',
+    packages=['reactor'],
+    package_dir={'': 'reactor'},
+    url='',
+    license='',
+    author='Reactor',
+    author_email='',
+    description=''
+)
